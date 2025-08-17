ed98084104ad85ab0fd314ffe82e4d79fa731bc6
Pedro Enderica <104291673+aMononito@users.noreply.github.com>
2025-08-17 14:34:39 -0400

    commit1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.torstig.striva',
+  appName: 'striva-test',
+  webDir: 'www'
+};
+
+export default config;
